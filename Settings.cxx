dc2e8363ff9328f29a8c17ebb8b17522ac5516fd
Francisco de Melo <francisco_melo_jr@hotmail.com>
2016-07-17 04:16:34 -0300

    Settings: addition of constants on the constructor

--- Settings.cxx
+++ Settings.cxx
@@ -5,8 +5,21 @@ ClassImp(Settings)
 
 Settings::Settings()
 {
-  cout << "Settings" << endl;
+       cout << "Settings" << endl;
 
+       gerator = 1;
+       energy = 2300;
+
+       xMinpt = 0;
+       xMaxpt =3;
+       xMin = -10;
+       xMax = 10;
+       div = 12;
+ //    lim = xMax - xMin;
+       y = xMin;
+       pInt = 100;
+ //    xi = xMin;
+       size = 54;
 }
 Settings::Method1()
 {
--- Settings.h
+++ Settings.h
@@ -7,7 +7,22 @@ class Settings : public TObject
 {
     public:
        Settings();
-       X();
+       Method1();
  
+    private:
+       int gerator;
+       double energy;
+
+       double xMinp;
+       double xMaxpt;
+       double xMin;
+       double xMax;
+       Double_t div;
+       double lim;
+       double y;
+       int pInt;
+       double xi; 
+       int size;
+
     ClassDef(Settings,1)
 };
