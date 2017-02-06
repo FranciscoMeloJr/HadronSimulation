3c19518ffb05adc31ebb467dd9afeda6f3833dbf
FranciscoMeloJr <francisco_melo_jr@hotmail.com>
2017-02-06 04:38:50 -0500

    initialize function

--- Simulation.h
+++ Simulation.h
@@ -1,17 +1,18 @@
-#ifndef Simulation_H
-#define Simulation_H
+#ifndef ROOT_Simulation_H
+#define ROOT_Simulation_H
 
+#ifndef ROOT_TObject
+#include "TObject.h"
 #endif
 
-
 using namespace std;
-
 //Class Equation:
 class Simulation : public TObject{
   public:
         Simulation();
         void read();
         void initialize();
+        virtual ~Simulation();
   private:
         int events;   
         double energy;  
@@ -20,15 +21,18 @@ class Simulation : public TObject{
         double xmaxpt;                          // Valor do limite máximo para pt
         double xmin;                          // X min para a integral de rapidez  
         double xmax;                           // X max para a integral de rapidez
-        const Double_t div;                     // Número de pontos para plotar no gráfico da rapidez
+        const Double_t div = 12;                     // Número de pontos para plotar no gráfico da rapidez
         double lim;                   // valor do limite de x
         double y;    
                         // valores para o eixo x 
         const int pint = 100;                       // pontos para a interpolação
         double xi;                           // valor para o eixo x da interpolação 
         const int size = 54;                        // Número de particulas  
-        double n[size];
-        int charge[size];
+        double* n;
+        double* mass;
+        int* charge;
 
   ClassDef(Simulation, 1)
 };
+
+#endif
--- Simulation_cxx.d
+++ Simulation_cxx.d
@@ -1,6 +1,16 @@
 
 # DO NOT DELETE
 
-./Simulation_cxx.so: Simulation.h
+./Simulation_cxx.so: Simulation.h /usr/include/root/TObject.h
+./Simulation_cxx.so: /usr/include/root/Rtypes.h /usr/include/root/RConfig.h
+./Simulation_cxx.so: /usr/include/root/RVersion.h
+./Simulation_cxx.so: /usr/include/root/DllImport.h
+./Simulation_cxx.so: /usr/include/root/Rtypeinfo.h
+./Simulation_cxx.so: /usr/include/root/snprintf.h /usr/include/root/strlcpy.h
+./Simulation_cxx.so: /usr/include/root/TGenericClassInfo.h
+./Simulation_cxx.so: /usr/include/root/TSchemaHelper.h
+./Simulation_cxx.so: /usr/include/root/TStorage.h
+./Simulation_cxx.so: /usr/include/root/TVersionCheck.h
+./Simulation_cxx.so: /usr/include/root/Riosfwd.h /usr/include/root/TBuffer.h
 ./Simulation_cxx.so: /usr/include/root/cintdictversion.h /usr/include/root/RVersion.h
 Simulation_cxx__ROOTBUILDVERSION= 5.34/30
